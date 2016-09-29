705f13a1d158064dc59cee40456b374c625dbb86
Murray Calavera <murray.calavera@gmail.com>
2016-09-29 21:26:16 +0100

    weston-launch: use custom error function
    error.h is a gnu extension and not available in other
    popular libcs like musl. This patch provides a replacement.
    
    Signed-off-by: Murray Calavera <murray.calavera@gmail.com>
    Reviewed-by: Eric Engestrom <eric.engestrom@imgtec.com>

--- libweston/weston-launch.c
+++ libweston/weston-launch.c
@@ -33,7 +33,6 @@
 #include <poll.h>
 #include <errno.h>
 
-#include <error.h>
 #include <getopt.h>
 
 #include <sys/types.h>
@@ -112,6 +111,25 @@ struct weston_launch {
 };
 
 union cmsg_data { unsigned char b[4]; int fd; };
+
+static void
+error(int status, int errnum, const char *msg, ...)
+{
+	va_list args;
+
+	fputs("weston-launch: ", stderr);
+	va_start(args, msg);
+	vfprintf(stderr, msg, args);
+	va_end(args);
+
+	if (errnum)
+		fprintf(stderr, ": %s\n", strerror(errnum));
+	else
+		fputc('\n', stderr);
+
+	if (status)
+		exit(status);
+}
 
 static gid_t *
 read_groups(void)
